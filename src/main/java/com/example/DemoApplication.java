8d32200f82567e95f19fd83d1d6a587adc97439d
Di Huynh <Di.Huynh@blackbaud.com>
2016-02-26 10:08:22 -0600

    messing around

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 		return "Hello Di";
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+//	}
 }
